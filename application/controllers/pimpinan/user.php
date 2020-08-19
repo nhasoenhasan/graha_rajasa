2b8d585cc3a8b93524b2bde064371fa744e218f6
Nur Hasan <nhasan716@gmail.com>
2020-08-20 02:52:33 +0700

    tes

--- application/controllers/pimpinan/user.php
+++ application/controllers/pimpinan/user.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class user extends CI_Controller {
+class User extends CI_Controller {
 	function __construct() {
         parent::__construct();
         $this->load->helper('url_helper');
